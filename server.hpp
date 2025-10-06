4716f693442e8b1bbbeb47c9fb727ea1d6d7f658
Titouan Voisin <tvoisin@z2r6p2.42lyon.fr>
2025-10-06 11:09:54 +0200

    uu

---
+++ server.hpp
@@ -0,0 +1,21 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h> 
+# include <poll.h>
+
+class Server{
+    private:
+        int _port;
+        std::string _password;
+        bool    _signal;
+    public:
+};
+
+#endif
