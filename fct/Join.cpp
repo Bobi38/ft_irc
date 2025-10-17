7694eba7051ec2651af1485d5b8f2110e4a27769
Ton Nom <ton@email.com>
2025-10-17 14:47:09 +0000

    tt

--- fct/Join.cpp
+++ fct/Join.cpp
@@ -1,2 +1,20 @@
 #include "Makerj.hpp"
 
+bool check_join(Request& rq){
+    for(int i = 0; rq[i]; i++){
+        if (tab[i][0] != "#")
+            return false;
+    }
+    return true;
+}
+
+bool init_chan_key(Request& rq,  std::vector<std::string name>& chan, std::vector<std::string key>& key){
+    
+}
+
+void exec_join(Request& rq, Server* server, Client* client){
+    std::vector<std::string name> chan;
+    std::vector<std::string key> key;
+    if (!client || !check_join(rq))
+
+}
