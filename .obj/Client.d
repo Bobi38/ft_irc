85914d8880c2b4d01e8e091d1616316c98ecc310
Ton Nom <ton@email.com>
2025-10-30 08:26:43 +0000

    fiix

--- .obj/Client.d
+++
@@ -1,6 +0,0 @@
-.obj/Client.o: app/Client.cpp app/include/Client.hpp \
- app/include/Channel.hpp app/include/Client.hpp app/include/Server.hpp
-app/include/Client.hpp:
-app/include/Channel.hpp:
-app/include/Client.hpp:
-app/include/Server.hpp:
--- .obj/Prefix.d
+++
@@ -1,2 +0,0 @@
-.obj/Prefix.o: cmd/Prefix.cpp cmd/./include/Prefix.hpp
-cmd/./include/Prefix.hpp:
