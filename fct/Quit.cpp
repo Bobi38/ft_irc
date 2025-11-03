e67abfa76155b3be79315ce20acabaaf1655a4dd
Ton Nom <ton@email.com>
2025-11-03 19:49:38 +0000

    tt

--- fct/Quit.cpp
+++ fct/Quit.cpp
@@ -1,19 +1,21 @@
 #include "Makerj.hpp"
 
-void fd_send(Client* clt, std::set<int>& list){
+void fd_send(Client* clt, std::set<int>& list, bool in){
 	for(size_t i = 0; clt->getChan(i); i++){
 		Channel* chan = clt->getChan(i);
+		if (chan->is_inv(clt->getNick()))
+			continue;
 		for(size_t y = 0;chan->getClient(y); y++){
 			int testfd = chan->getClient(y)->getfd();
-			// if (clt->getfd() != testfd)
-			list.insert(testfd);
+			if (in == false && testfd != clt->getfd())
+				list.insert(testfd);
 		}
 	}
 }
 
 void send_all_chan(std::string msg, Client* clt, Server* serv){
 	std::set<int> list;
-	fd_send(clt, list);
+	fd_send(clt, list, true);
 	for(std::set<int>::iterator it = list.begin(); it != list.end(); it++){
 		Client *tmp;
 		tmp = serv->find_fd(*it);
@@ -23,7 +25,7 @@ void send_all_chan(std::string msg, Client* clt, Server* serv){
 
 void send_all(Request& rq, Client* clt){
 	std::set<int> list;
-	fd_send(clt, list);
+	fd_send(clt, list, false);
 	for(std::set<int>::iterator it = list.begin(); it != list.end(); it++)
 		send_msg(*it, rq.getMsg());
 }
