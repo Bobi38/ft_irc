6ce0994a55e5ccad0813c9b69c0deae0846dbf6a
Nicolas Rochedy <niroched@z2r2p6.42lyon.fr>
2025-11-12 12:06:53 +0100

    msg kick

--- fct/Kick.cpp
+++ fct/Kick.cpp
@@ -9,20 +9,20 @@ void exec_kick(Request& rq, Server* server, Client* client){
 
 	TChan = server->find_channel(rq[1]);
 	if (!TChan){
-		client->rcvMsg("403 " + rq[1] + " :No such channel");
+		client->rcvMsg(":server 403 " + rq[1] + " :No such channel");
 		return ;
 	}
 	TClt = TChan->return_client(rq[2]);
 	if (!TClt || (TChan->getStatutClt(TClt) != PRESENT && TChan->getStatutClt(TClt) != CHANOP && TChan->getStatutClt(TClt) != BOT)){
-		client->rcvMsg("442 " + rq[2] + " :You're not on that channel");
+		client->rcvMsg(":server 442 " + rq[2] + " " + rq[1]+ " :is not on that channel");
 		return ;
 	}
 	if (TChan->is_in(client->getNick()) == false){
-		client->rcvMsg("442 " + client->getNick() + " :You're not on that channel");
+		client->rcvMsg(":server 442 " + client->getNick() + " :You're not on that channel");
 		return ;	   
 	}
 	if (TChan->getStatutClt(client) != CHANOP){
-		client->rcvMsg("482 " + client->getNick() + " :You're not channel operator");
+		client->rcvMsg(":server 482 " + rq[2] + " " + rq[1] + " :You're not channel operator");
 		return ;
 	}
 	TChan->chan_msg(client->getMe() + " KICK " + TChan->getName() + " " + TClt->getNick());
